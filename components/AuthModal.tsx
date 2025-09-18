0e53c7d6c5c06bd76f4c22c3281055e9d68ea6c9
Firenze Higa Putra <firenzehiga@gmail.com>
2025-09-18 22:08:55 +0700

    refactor: replace img with Image component from next/image for Google logo

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -11,6 +11,7 @@ import {
 import { useAuth } from "@/contexts/AuthContext";
 import toast from "react-hot-toast";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 interface AuthModalProps {
 	isOpen: boolean;
@@ -201,7 +202,7 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
 								}}
 								disabled={loading}
 								className="w-full bg-white hover:bg-gray-300 text-gray-900 py-2 px-3 rounded-md font-semibold shadow-lg hover:shadow-xl disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 flex items-center justify-center gap-2">
-								<img
+								<Image
 									src="/images/google.png"
 									alt="Google"
 									className="w-10 h-10"
@@ -296,7 +297,7 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
 								}}
 								disabled={loading}
 								className="w-full bg-white text-gray-900 py-2 px-3 hover:bg-gray-300 rounded-md font-semibold shadow-lg hover:shadow-xl disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 flex items-center justify-center gap-2">
-								<img
+								<Image
 									src="/images/google.png"
 									alt="Google"
 									className="w-10 h-10"
