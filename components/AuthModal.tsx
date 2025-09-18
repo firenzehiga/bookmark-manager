1de43a8c4025e4d408cf912ab379135a328bebb8
Firenze Higa Putra <firenzehiga@gmail.com>
2025-09-18 22:10:57 +0700

    refactor: add width and height attributes to Google logo Image component in AuthModal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -203,6 +203,8 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
 								disabled={loading}
 								className="w-full bg-white hover:bg-gray-300 text-gray-900 py-2 px-3 rounded-md font-semibold shadow-lg hover:shadow-xl disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 flex items-center justify-center gap-2">
 								<Image
+									width={50}
+									height={50}
 									src="/images/google.png"
 									alt="Google"
 									className="w-10 h-10"
@@ -298,6 +300,8 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
 								disabled={loading}
 								className="w-full bg-white text-gray-900 py-2 px-3 hover:bg-gray-300 rounded-md font-semibold shadow-lg hover:shadow-xl disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 flex items-center justify-center gap-2">
 								<Image
+									width={50}
+									height={50}
 									src="/images/google.png"
 									alt="Google"
 									className="w-10 h-10"
